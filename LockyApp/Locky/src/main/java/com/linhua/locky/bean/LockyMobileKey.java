32520fc8ed516a44d3960170eeb8e8dc68425c7c
sherwinstar <slzhouzsl@163.com>
2022-11-09 16:47:52 +0800

    refactor sdk structures

---
+++ LockyApp/Locky/src/main/java/com/linhua/locky/bean/LockyMobileKey.java
@@ -0,0 +1,23 @@
+package com.linhua.locky.bean;
+
+public class LockyMobileKey {
+
+    private String token;
+    private String tenantId;
+
+    public String getToken() {
+        return tenantId;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+}
--- LockyApp/app/src/main/java/com/linhua/lockyapp/MainActivity.java
+++ LockyApp/app/src/main/java/com/linhua/lockyapp/MainActivity.java
@@ -1,34 +1,145 @@
 package com.linhua.lockyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import com.linhua.locky.LockyActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.text.Editable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.linhua.locky.api.ApiAuthManager;
+import com.linhua.locky.bean.TokenModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView enterTextView;
+    private static final String TAG = "LockyActivity";
+    private EditText emailEditText;
+    private Button startVerifyBtn;
+    private EditText codeEditText;
+    private Button verifyBtn;
+    private TextView tokenTextView;
+    private Button mobileKeysBtn;
+    private Button alLocksBtn;
+    private LinearLayout locksLinearLayout;
+
+    private TokenModel tokenModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+
     }
 
     private void initView() {
-        enterTextView = findViewById(R.id.tv_enter);
-        enterTextView.setOnClickListener(new View.OnClickListener() {
+        emailEditText = findViewById(R.id.et_email);
+        startVerifyBtn = findViewById(R.id.btn_email);
+        codeEditText = findViewById(R.id.et_code);
+        verifyBtn = findViewById(R.id.btn_code);
+        tokenTextView = findViewById(R.id.lb_token);
+        mobileKeysBtn = findViewById(R.id.btn_keys);
+        alLocksBtn = findViewById(R.id.btn_locks);
+        locksLinearLayout = findViewById(R.id.locks_linear_layout);
+        startVerifyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent lockyIntent = new Intent(MainActivity.this, LockyActivity.class);
-                startActivity(lockyIntent);
+                startVerify();
             }
         });
+        verifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                verify();
+            }
+        });
+
+        mobileKeysBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getMobileKeys();
+            }
+        });
+
+        startVerifyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getAllLocks();
+            }
+        });
+
+    }
+
+    private void startVerify() {
+        String email = emailEditText.getText().toString().trim();
+        if (email.isEmpty()) {
+            return;
+        }
+        Call<Void> call = ApiAuthManager.getInstance().getHttpApi().startVerify(email, "mobilekey");
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                Log.v(TAG, "success");
+                // successful
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                Log.v(TAG, "error");
+                // fail
+            }
+        });
+    }
+
+    private void verify() {
+        String email = emailEditText.getText().toString().trim();
+        if (email.isEmpty()) {
+            return;
+        }
+        String code = codeEditText.getText().toString().trim();
+        if (code.isEmpty()) {
+            return;
+        }
+
+        Call<TokenModel> call = ApiAuthManager.getInstance().getHttpApi().verify(email, code, "mobilekey");
+        call.enqueue(new Callback<TokenModel>() {
+            @Override
+            public void onResponse(Call<TokenModel> call, Response<TokenModel> response) {
+                tokenModel = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<TokenModel> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void getMobileKeys() {
+
+    }
+
+    private void getAllLocks() {
+
+    }
+
+
+    private View createLockView() {
+        View view = LayoutInflater.from(this.getApplicationContext()).inflate(R.layout.layout_lock, null, false);
+        locksLinearLayout.addView(view);
+        return view;
     }
 
 }
